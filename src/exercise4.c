b957d119f73c509d827228f4e23488d52238f7cb
polinad12 <dpolina373@gmail.com>
2025-10-31 02:21:45 +0700

    Update exercise4.c

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
+#include <stdlib.h>
+int main(int argc, char* argv[]) {
+    int array[10];
+    int X;
+    int K;
+    X = atof(argv[1]);
+    K = atoi(argv[2]);
+    for(int i = 0; i < 10; i++) {
+        scanf("%d", &array[i]);
+    }
+    for(int i = 9; i > K; i--) {
+        array[i] = array[i-1];
+    }
+    array[K] = X;
+    for(int i = 0; i < 10; i++) {
+        printf("%.d", array[i]);  
+        if(i < 9) printf(" ");
+    }
+    printf("\n");
     return 0;
 }
