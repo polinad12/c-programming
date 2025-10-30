bc7ad81e9ce73e6d212395436bcbcf5304e11c4a
polinad12 <dpolina373@gmail.com>
2025-10-31 02:30:47 +0700

    Update exercise3.c

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char* argv[]) {
-    int arr[10];
+    double array[10];
     int K;
     K = atoi(argv[1]);
     for(int i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%lf", &array[i]);
     }
     K = K % 10;
     if(K < 0) K += 10;
     for(int i = 0; i < 10; i++) {
         int index = (i - K + 10) % 10;
-        printf("%d", arr[index]);  
+        printf("%.3f", array[index]); 
         if(i < 9) printf(" ");
     }
     printf("\n");
