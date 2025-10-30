6d6391d301af602dd988633027756527cb0b7239
polinad12 <dpolina373@gmail.com>
2025-10-31 02:08:56 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    double array[10];
+    double arr[10];
     double sum = 0.0;
     
-    // Чтение массива
     for(int i = 0; i < 10; i++) {
-        scanf("%lf", &array[i]);
-        sum += array[i];
+        scanf("%lf", &arr[i]);
+        sum += arr[i];
     }
     
-    // Вычисление среднего с повышенной точностью
-    double average = sum / 10.0;
-    
-    // Округление до двух знаков после запятой
-    average = round(average * 100) / 100;
-    
+    double average = sum / 100.0;
     printf("%.2f\n", average);
     
     return 0;
