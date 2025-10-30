221c69848ef909f1781b0cca7b52b0183be4b286
polinad12 <dpolina373@gmail.com>
2025-10-31 02:16:56 +0700

    Update exercise3.c

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
+#include <stdlib.h>
+int main(int argc, char* argv[]) {
+    double arr[10];
+    int K;
+    K = atoi(argv[1]);
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &arr[i]);
+    }
+    K = K % 10;
+    if(K < 0) K += 10;
+    for(int i = 0; i < 10; i++) {
+        int index = (i - K + 10) % 10;
+        printf("%.3f", arr[index]);  
+        if(i < 9) printf(" ");
+    }
+    printf("\n");
     return 0;
 }
