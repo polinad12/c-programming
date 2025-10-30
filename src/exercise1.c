3a574ba801d76a8ff74ad0acd58f1150d39b19f5
polinad12 <dpolina373@gmail.com>
2025-10-31 02:02:23 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    double arr[10];
+    double sum = 0.0;
+    
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &arr[i]);
+        sum += arr[i];
+    }
+    
+    double average = sum / 10.0;
+    printf("%.2f\n", average);
+    
     return 0;
 }
