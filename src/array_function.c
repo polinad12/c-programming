126940bdb254fb70d87cfc42401c6e3a6d5281df
Polina D <dpolina373@gmail.com>
2025-10-31 03:58:30 +0700

    correct

--- src/array_function.c
+++ src/array_function.c
@@ -1,4 +1,4 @@
-﻿#include "array_function.h"
+﻿#include "../include/array_function.h"
 
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     // Копируем первый массив
